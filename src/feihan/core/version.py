edd8a7c9f11a507f4e614e1179cd0032cd9bdd56
xalanq <xalanq@gmail.com>
2026-04-01 20:32:09 +0800

    release: 发布 0.28.108

--- src/feihan/core/version.py
+++ src/feihan/core/version.py
@@ -1,5 +1,5 @@
 # Copyright (c) 2026 上海飞函安全科技有限公司 (Shanghai Feihan Security Technology Co., Ltd.)
 # SPDX-License-Identifier: Apache-2.0
 
-VERSION = "0.28.107"
-USER_AGENT = "feihan-openapi-sdk-python/0.28.107"
+VERSION = "0.28.108"
+USER_AGENT = "feihan-openapi-sdk-python/0.28.108"
